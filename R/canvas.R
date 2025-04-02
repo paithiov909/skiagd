6da07460dd157028af586e246461fc4b55cca09f
paithiov909 <am11.44@gmail.com>
2025-04-02 13:56:12 +0900

    refactor: rename `size` with `canvas_size`

--- R/canvas.R
+++ R/canvas.R
@@ -5,12 +5,13 @@
 #' @param fill RGBA representation of a color.
 #' This can be specified using named colors or hexadecimal color codes,
 #' which are converted internally using [grDevices::col2rgb()].
-#' @param size Integers of length 2; canvas size.
+#' @param canvas_size Integers of length 2.
 #' @returns A raw vector of picture.
 #' @export
-canvas <- function(fill = "transparent", size = paint()[["canvas_size"]]) {
+canvas <- function(fill = "transparent",
+                   canvas_size = paint()[["canvas_size"]]) {
   if (!is.numeric(fill)) {
     fill <- col2rgba(fill)
   }
-  sk_absolute_fill(size, fill)
+  sk_absolute_fill(canvas_size, fill)
 }
