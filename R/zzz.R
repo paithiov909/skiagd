ae91a67f1b16bdbe569a33769e30f774c32cc856
paithiov909 <am11.44@gmail.com>
2025-04-16 22:51:54 +0900

    add '%timer%'

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,38 @@
 .onUnload <- function(libpath) {
   library.dynam.unload("skiagd", libpath)
 }
+
+#' Log execution time of an expression
+#'
+#' An operator to evaluate an expression
+#' while logging its execution time for debugging.
+#'
+#' @details
+#' `lhs %timer% rhs` evaluates `rhs` inside a [system.time] call,
+#' assigns the execution time to the variable `time`,
+#' and tries to evaluate `lhs` in an environment where `time` exists.
+#' In doing so, failure to evaluate the left-hand side
+#' does not result in an error.
+#'
+#' @param lhs An expression.
+#' @param rhs An expression.
+#' @returns Values from evaluated `rhs` is returned invisibly.
+#' @export
+#' @examples
+#' print(time) %timer% {
+#'  rnorm(10) ^2
+#' }
+`%timer%` <- function(lhs, rhs) {
+  lhs <- rlang::enquo(lhs)
+  rhs <- rlang::enquo(rhs)
+  time <- system.time({
+    ret <- rlang::eval_tidy(rhs)
+  })
+  rlang::try_fetch(
+    rlang::eval_tidy(lhs, data = list(time = time[["elapsed"]])),
+    error = function(e) {
+      rlang::warn("Failed to evaluate lhs.", parent = e)
+    }
+  )
+  invisible(ret)
+}
