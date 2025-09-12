f1df3fc13d423e1d1cd2ed1c48e614eebcb8fb3e
paithiov909 <am11.44@gmail.com>
2025-09-12 13:17:31 +0900

    fix: replace 'get_widths_bounds' call

--- src/rust/src/paint_attrs/mod.rs
+++ src/rust/src/paint_attrs/mod.rs
@@ -102,9 +102,11 @@ impl PaintAttrs {
     }
     pub fn reset_blur(&mut self, sigma: f64) {
         // NOTE: `skia_safe::MaskFilter::blur` returns an option. `None` is returned if the sigma is less than 0.
-        self.paint.set_mask_filter(
-            skia_safe::MaskFilter::blur(self.blur_style, sigma as f32, false)
-        );
+        self.paint.set_mask_filter(skia_safe::MaskFilter::blur(
+            self.blur_style,
+            sigma as f32,
+            false,
+        ));
     }
 }
 
@@ -148,22 +150,15 @@ pub fn assert_len(name: &str, expected: usize, actual: usize) -> anyhow::Result<
 fn sk_get_text_info(text: savvy::StringSexp, props: PaintAttrs) -> savvy::Result<savvy::Sexp> {
     let typeface = font::match_family_style(props.font_family.as_str(), props.font_face)?;
     let font = skia_safe::Font::from_typeface(&typeface, props.font_size);
+
     let mut width = savvy::OwnedRealSexp::new(text.len())?;
     let mut id = savvy::OwnedIntegerSexp::new(text.len())?;
     let mut n_chars = savvy::OwnedIntegerSexp::new(text.len())?;
     for (i, t) in text.iter().enumerate() {
         let glyph_ids = font.text_to_glyphs_vec(t.to_string());
         let n = font.count_text(t.to_string());
-        let mut width_ptrs: Vec<f32> = Vec::new();
-        width_ptrs.resize(n, 0.0);
-        let width_ptrs = width_ptrs.as_mut_slice();
-        font.get_widths_bounds(
-            &glyph_ids.as_slice(),
-            Some(width_ptrs),
-            None,
-            Some(&props.paint),
-        );
-        width.set_elt(i, width_ptrs.iter().map(|x| *x as f64).sum::<f64>())?;
+        let (w, _) = font.measure_text(glyph_ids.as_slice(), Some(&props.paint));
+        width.set_elt(i, w as f64)?;
         id.set_elt(i, i as i32)?;
         n_chars.set_elt(i, n as i32)?;
     }
